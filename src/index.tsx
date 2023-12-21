3613436f0d3cc134187ac07506d8145be9c664fc
Solomiya <solomiyajones@gmail.com>
2023-12-21 01:59:46 -0500

    added new tsx files to layout folder: app,header,mainnd footer

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './header'
-import Content from './content'
+import App from './layout/App'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-const App = () => {
-    return (
-        <>
-            <Header title="App.js" num={10} />
-            <Header title="React.js" />
-            <Content p1="Hello wolrd 1" p2="Hello world 2" bgColor="purple" />
-            <Content p1="Hello wolrd 3" p2="Hello world 4" bgColor="red" />
-            <Content p1="Hello wolrd 5" p2="Hello world 6" bgColor="yellow" />
-        </>
-    )
-}
 root.render(
     <React.StrictMode>
         <App />
