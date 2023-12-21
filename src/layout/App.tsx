713f056d33f5685207618874a25c459efec6abab
Solomiya <solomiyajones@gmail.com>
2023-12-21 02:26:08 -0500

    updated styles by using css baseline

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,12 +1,13 @@
 import Footer from './Footer'
 import Header from './Header'
 import Main from './Main'
-
+import CssBaseline from '@mui/material/CssBaseline'
 type Props = {}
 
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
